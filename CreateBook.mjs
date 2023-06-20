6c55ad3b73c74706d1927b9b39096f2be5148354
Miguel Guzmán <99280294+gradiux-miko@users.noreply.github.com>
2023-06-20 16:04:49 -0600

    Add files via upload
    AWS Lambda code for registering items in Dynamo DB using NodeJs

---
+++ CreateBook.mjs
@@ -0,0 +1,35 @@
+// AWS Lambda code for registering items in Dynamo DB using NodeJs
+// https://www.youtube.com/watch?v=MxSF2GPz4HE
+
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { randomUUID } from "crypto";
+
+const ddbDocClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const handler = async (event, context) => {
+    try {
+        const book = JSON.parse(event.body);
+        
+        const newBook = {
+            ...book,
+            id: randomUUID(),
+        };
+        await ddbDocClient.send(new PutCommand({
+            TableName: "books",
+            Item: newBook,
+        }));
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify(newBook),
+        };
+    }
+    catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: error.message }),
+        };
+    }
+};
